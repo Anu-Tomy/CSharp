1ba62e8b0c2612b9bee89f83aac5f049d4c35e33
Anu Tomy <anuatomy@gmail.com>
2017-11-20 23:22:16 -0500

    Examples of Referencetype and Valuetype using methods

---
+++ ReferenceTypesandValueTypes2/ReferenceTypesandValueTypes2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+
+namespace ReferenceTypesAndValueTypes2
+
+{
+    public class Person
+
+    {
+        public int Age;
+    }
+
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+
+            var number = 1;
+            Increment(number); 
+            Console.WriteLine(number); // 1
+                        
+            var person = new Person() { Age = 20 };
+            MakeOld(person); // same object as in previous line 
+            Console.WriteLine(person.Age);
+        }
+
+        public static void Increment(int number)
+
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+
+        {
+            person.Age += 10;
+        }
+
+    }
+
+}
