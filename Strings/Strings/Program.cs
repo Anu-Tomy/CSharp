de6f17bc9bc56bb4a2f59bd6d8f5b514d28a367f
Anu Tomy <anuatomy@gmail.com>
2017-11-20 19:29:48 -0500

    Examples of string.Format( ) and verbatim strings (@)

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "John";
+            var lastName = "Smith";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullname = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] { "John", "Jack", "Mary"};
+
+            var text = @"Hi John
+Look into the following paths
+C:\folder1\folder2
+C:\folder3\folder4";
+            Console.WriteLine(text);
+        }
+    }
+}
