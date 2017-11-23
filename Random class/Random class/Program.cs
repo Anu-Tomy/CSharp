a45d36e2ef4df13ea927ebc9bae172bcbb8118a3
Anu Tomy <anuatomy@gmail.com>
2017-11-22 22:34:02 -0500

    Example of Random class

---
+++ Random class/Random class/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            //random numbers
+            //for(var i = 0; i < 10; i++)
+            //    Console.WriteLine(random.Next(1, 10));
+
+            //random string
+            const int passwordLength = 10;
+            
+            var buffer = new char[passwordLength];
+            for (var i = 0; i < passwordLength; i++)
+                buffer[i] = (char)('a' + random.Next(0, 26));
+
+            var password = new string(buffer);
+            Console.WriteLine(password);
+        }
+    }
+}
