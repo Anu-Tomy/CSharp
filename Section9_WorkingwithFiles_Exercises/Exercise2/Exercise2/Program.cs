ea11bdf85b436886015982c224657d01a9357b80
Anu Tomy <anuatomy@gmail.com>
2017-11-29 23:46:25 -0500

    Complete a program to display the longest word in a file

---
+++ Section9_WorkingwithFiles_Exercises/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads a text file and displays the longest word in the file.
+            var path = @"C:\Documents\samplefile.txt";
+            if(File.Exists(path))
+            {
+                var content = File.ReadAllText(path);
+                var wordArray = content.Split(' ');
+                var wordlength = 1;
+                string longWord = " ";
+                foreach(string word in wordArray)
+                {
+                    if(word.Length > wordlength)
+                    {
+                        longWord = word;
+                    }
+                }
+                Console.WriteLine("Longest word is : " + longWord);
+            }
+        }
+    }
+}
